65030871a59b463f59716cd2006d2f039f6ee4a3
fwcd <fwcdmail@gmail.com>
2022-03-30 21:05:17 +0200

    Set up argument parsing

--- appicongen/__init__.py
+++ appicongen/__init__.py
@@ -1,2 +1,11 @@
+import argparse
+
 def main():
+    parser = argparse.ArgumentParser(description='Tool for generating macOS/iOS app icons')
+    parser.add_argument('--macos', action='store_true', help='Generate macOS icons')
+    parser.add_argument('--ios', action='store_true', help='Generate iOS icons')
+    parser.add_argument('--ipados', action='store_true', help='Generate iPadOS icons')
+    parser.add_argument('--watchos', action='store_true', help='Generate watchOS icons')
+
+    args = parser.parse_args()
     print("Hello")
